ca7a0311a368a31b5184bed9ff2047debf3c6a19
westline95 <westline.1995@gmail.com>
2025-08-01 18:03:12 +0700

    fix overlay sidebar

--- src/pages/BaseLayout.jsx
+++ src/pages/BaseLayout.jsx
@@ -5,9 +5,12 @@ import Header from '../parts/Header';
 export default function BaseLayout({children}){
     const [ isOpen, setOpen ] = useState(false);
     const sidebarOverlayResp = useRef(null);
+    const mainOverlayResp = useRef(null);
 
     useEffect(() => {
         if(sidebarOverlayResp.current.classList.contains("active")){
+            mainOverlayResp.current.classList.add("freeze");
+            console.log(mainOverlayResp)
             sidebarOverlayResp.current.addEventListener("click", () => {
                 setOpen(false);
             })
@@ -15,16 +18,16 @@ export default function BaseLayout({children}){
     },[sidebarOverlayResp.current])
 
     return(
-        <div className="wrapper">
-            <div className="d-flex position-relative">
+        <div className="wrapper" style={{overflowY:'unset'}}>
+            <div className="d-flex position-relative" style={{overflowY:'unset'}}>
                 <Sidebar show={isOpen} />
-                <main className={`main-content ${isOpen ? "active" : ""}`}>
+                <main ref={mainOverlayResp} className={`main-content ${isOpen ? "active" : ""}`} style={{overflowY:'unset'}}>
                     <Header onClick={() => setOpen((p) => !p)} />
                     {/* //components from all routes */}
                     {children} 
                 </main>
-            </div>
             <div ref={sidebarOverlayResp} className={`vertical-background ${isOpen ? "active " : ''}`}></div>
+            </div>
         </div>
     )
 }
